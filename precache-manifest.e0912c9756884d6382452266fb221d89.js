716bdfbc917d16e274a8c2266f36a98189c85620
serch-engine4 <sergio.martinezc@outlook.com>
2020-06-02 12:07:51 -0500

    Updates

---
+++ precache-manifest.e0912c9756884d6382452266fb221d89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d408100f84fe55c05e85d6e9a1259ecd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc7d7084c39db3f6ab14",
+    "url": "/monsters-rolodex/static/css/main.0902d2d8.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7d7084c39db3f6ab14",
+    "url": "/monsters-rolodex/static/js/main.4e9c10a2.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
